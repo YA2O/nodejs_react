2254eed4f30d254611e81d50b712ad06ad118f52
YA2O <yann.ollivier@itravel.de>
2023-11-09 17:25:43 +0100

    ZZZ

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          YYY Please edit <code>src/App.js</code> and save to reload.
+          ZZZ Please edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
